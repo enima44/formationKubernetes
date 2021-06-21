55a0d484761e8b074eb21ed3d2c67a9c69125777
ihab <ihab@utopios.net>
2021-06-21 19:23:14 +0100

    suite exercice service kubernetes

--- exemple-app-nodejs/index.js
+++ exemple-app-nodejs/index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const os = require('os')
 const app = express()
 
+const mysql = require('mysql')
+
+const db = mysql.createConnection({
+    host: "nom_du_service",
+    user: '',
+    password : ''
+})
+
 app.get('/', (req, res) => {
+    db.connect((err) => {
+    })
     res.end("Hello from our pod "+ os.hostname());
 })
 
 app.listen(3000, () => {
-    
+
 })
